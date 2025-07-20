c10befb494bf881e3bf54e5c125d98592cb4cbb8
a.nikitin <pautinaweb@mail.ru>
2025-07-20 19:27:06 +0300

    Чистый битрикс

---
+++ .section.php
@@ -0,0 +1,9 @@
+<?
+$sSectionName = 'Главная';
+$arDirProperties = array(
+	'title' => 'Title Мой сайт',
+	'description' => 'Description Мой сайт',
+	'keywords' => 'Keywords Мой сайт',
+	'robots' => 'index, follow'
+);
+?>
