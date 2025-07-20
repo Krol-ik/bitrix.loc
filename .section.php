73bcaaf8f84225981f0e9b213c4109d890b03091
Alexander Nikitin <krol-88@bk.ru>
2025-07-20 19:14:07 +0300

    Delete .section.php

--- .section.php
+++
@@ -1,9 +0,0 @@
-<?
-$sSectionName = "Главная";
-$arDirProperties = Array(
-   "description" => "Description",
-   "keywords" => "Keywords",
-   "TITLE" => "Контактная информация",
-   "ROBOTS" => "index, follow"
-);
-?>
